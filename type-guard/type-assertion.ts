a089e65ad84bd5e558e1332e6ba7653d82865ea3
AyamotoKohei <koh14taka2@gmail.com>
2021-11-14 19:04:26 +0900

    add: 型ガードの学習

--- type-guard/type-assertion.ts
+++ type-guard/type-assertion.ts
@@ -1,1 +1,7 @@
 type User = { username: string; address: { zipcode: string; town: string; } };
+
+const str = `{"username": "patty", "town": "Maple Town"}`;
+const date: unknown = JSON.parse(str);
+const user = date as User;
+
+console.log(user.address.town); // TypeError: Cannot read property 'town' of undefined
