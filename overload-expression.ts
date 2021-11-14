8e451030a93a416921728d63940334f47f80a564
AyamotoKohei <koh14taka2@gmail.com>
2021-11-14 17:08:17 +0900

    add: 関数のオーバーロードの学習

---
+++ overload-expression.ts
@@ -0,0 +1,19 @@
+type Transform = {
+  (): void;
+  (item: Brooch): void;
+  (item: Compact): void;
+};
+
+const transform: Transform = (item?: Brooch | Compact): void => {
+  if (item instanceof Brooch) {
+    console.log("Moon crystal power 💎, make up!!");
+  } else if (item instanceof CosmicCompact) {
+    console.log("Moon cosmic power 💖, make up!!");
+  } else if (item instanceof CrisisCompact) {
+    console.log("Moon crisis 🏆, make up!");
+  } else if (!item) {
+    console.log("Moon prisim power 🌙, make up!");
+  } else {
+    console.log("Item is fake...😣");
+  }
+};
