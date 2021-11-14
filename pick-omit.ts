4dd74367e787550945fff1f0361cc81e5fc42926
AyamotoKohei <koh14taka2@gmail.com>
2021-11-14 16:08:56 +0900

    add: 組み込みユーティリティの学習

---
+++ pick-omit.ts
@@ -0,0 +1,8 @@
+type Todo = {
+  title: string;
+  description: string;
+  isDone: boolean;
+};
+
+type PickedTodo = Pick<Todo, 'title' | 'isDone'>; // { title: string; isDone: boolean }
+type OmittedTodo = Omit<Todo, 'description'>; // { title: string; isDone: boolean }
